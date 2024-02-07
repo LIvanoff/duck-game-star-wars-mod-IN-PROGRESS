3ec7a48916554860e15847eee411ea5b5339b09d
LIvanoff <drfrily@gmail.com>
2024-02-07 23:12:01 +0300

    update weaponthrowable.py

--- gameobjects/weaponthrowable.py
+++ gameobjects/weaponthrowable.py
@@ -1,9 +1,29 @@
-from weapons import Weapon
+import pygame
+
+from gameobjects.weapons import Weapon
 
 
 class WeaponThrowable(Weapon):
-    def __init__(self, game, type: str, pos: tuple, size: tuple, damage: int = 10, decay: int = 10, ammo_num: int = 100) -> None:
-        super().__init__(game, type, pos, size, damage, decay, ammo_num)
+    def __init__(self, game, type: str, pos: tuple, size: tuple, statsDict: dict) -> None:
+        super().__init__(game, type, pos, size, statsDict)
+        self.exlosion_time = statsDict['explosion_time']
+        self.radius = statsDict['radius']
+        self.shot_sound = statsDict['shot_sound_path']
+        self.exlosion_sound = statsDict['explosion_sound_path']
 
 
-    
+class Grenade(WeaponThrowable):
+    def __init__(self, game,
+                 type: str,
+                 pos: tuple,
+                 size: tuple,
+                 statsDict: dict
+                 ):
+        super().__init__(game, type, pos, size, statsDict)
+
+    def exlosion(self):
+        pygame.mixer.music.load(self.exlosion_sound)
+        pygame.mixer.music.play()
+
+    def throw(self):
+        pass
