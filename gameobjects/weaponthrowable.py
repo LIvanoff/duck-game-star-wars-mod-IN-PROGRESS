cad70643d07d5a3ba7433139dc8aae8daf53a9c1
LIvanoff <drfrily@gmail.com>
2024-02-08 00:25:25 +0300

    add grenade throwing

--- gameobjects/weaponthrowable.py
+++ gameobjects/weaponthrowable.py
@@ -1,15 +1,19 @@
 import pygame
 
 from gameobjects.weapons import Weapon
+from config import WIDTH, HEIGHT
 
 
 class WeaponThrowable(Weapon):
     def __init__(self, game, type: str, pos: tuple, size: tuple, statsDict: dict) -> None:
         super().__init__(game, type, pos, size, statsDict)
+        self.gravity = 9.8
         self.exlosion_time = statsDict['explosion_time']
         self.radius = statsDict['radius']
         self.shot_sound = statsDict['shot_sound_path']
         self.exlosion_sound = statsDict['explosion_sound_path']
+        self.isThrow = False
+        self.isBlowup = False
 
 
 class Grenade(WeaponThrowable):
@@ -20,10 +24,23 @@ class Grenade(WeaponThrowable):
                  statsDict: dict
                  ):
         super().__init__(game, type, pos, size, statsDict)
+        self.vel_y = -11
+        self.speed = 7
 
     def exlosion(self):
         pygame.mixer.music.load(self.exlosion_sound)
         pygame.mixer.music.play()
+        return True
 
-    def throw(self):
-        pass
+    def throw(self, direction):
+        self.vel_y += self.gravity
+        dx = direction * self.speed
+        dy = self.vel_y
+
+        # update grenade position
+        self.pos[0] += dx
+        self.pos[1] += dy
+
+        self.exlosion_time -= 1
+        if self.exlosion_time <= 0:
+            self.isBlowup = self.exlosion()
