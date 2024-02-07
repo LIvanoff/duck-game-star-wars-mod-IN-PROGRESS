56d092255b430a50b0b17a35eb5d168cfbb0155b
LIvanoff <drfrily@gmail.com>
2024-02-07 19:24:37 +0300

    update config.py

--- config.py
+++ config.py
@@ -23,23 +23,27 @@ ROUNDS = 5
 
 WEAPONS = {
     'dlt-19': {
+        'name': 'dlt-19',
         'damage': 100,
         'ammo_num': 3,
         'decay': 1.5,
         'path': 'assets/images/weapons/dlt19.png'
     },
     'e-11': {
+        'name': 'e-11',
         'damage': 30,
         'ammo_num': 20,
         'decay': 0.2,
         # 'overheating_time': ,
         'path': 'assets/images/weapons/e11.png'},
     'dl-44': {
+        'name': 'dl-44',
         'damage': 50,
         'ammo_num': 10,
         'decay': 0.7,
         'path': 'assets/images/weapons/dl44.png'},
     'sonic imploder': {
+        'name': 'sonic imploder',
         'damage': 100,
         'radius': 20,
         'explosion_time': 3.5,
